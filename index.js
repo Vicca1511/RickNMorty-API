039b75183d511ac78680687c71a5bb72327840fa
Lucas Viccari <lviccari4@gmail.com>
2022-07-17 18:39:44 -0300

    feat: adding a new user route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectToDb = require('./src/database/database');
 const routes = require('./src/Routes/character.route')
-
+const userRoutes = require('./src/users/user.route');
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/character", routes)
+app.use('/character', routes)
+app.use('/users', userRoutes)
 
 connectToDb();
 
--- src/Models/Character.js
+++ src/Models/Character.js
@@ -8,14 +8,18 @@ const characterScheme = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
+    // selected: false,
   },
   photo: {
     type: String,
